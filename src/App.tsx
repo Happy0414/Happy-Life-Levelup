9c3683c56a22a9108ff5b9059fa29920cd3f1349
Ayumu Fujimoto <fujimotoayumu0414@icloud.com>
2026-04-15 00:19:28 +0900

    route bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+//import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import './App.css'
 import ExpPage from './components/ExperiencePage'
-import Graph from './components/graph'
+//import Graph from './components/graph'
 
 
 function App() {
